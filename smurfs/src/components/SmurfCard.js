6567dfaac06f621d01657f1a0f0b1e8212a25f40
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-16 08:52:11 -0700

    Smurf data pulling through from API

---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Smurf = props => {
+    return (
+      <div className="smurf-card">      
+        <h3>SMURF CARD HERE</h3>
+        <p>Name: {props.name}</p>
+      </div>
+    );
+  };
+  
+  
+  export default Smurf;
